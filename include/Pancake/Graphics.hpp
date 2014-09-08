7aa709061f8de3e52248282ac5f09c5a2659b469
Dante12129 <wolfdan@cox.net>
2014-09-07 22:18:50 -0400

    Module Header Uploads
    Added headers than include all the headers for a module.

---
+++ include/Pancake/Graphics.hpp
@@ -0,0 +1,8 @@
+#ifndef GRAPHICS_HPP
+#define GRAPHICS_HPP
+
+#include <Pancake/Graphics/Color.hpp>
+#include <Pancake/Graphics/Image.hpp>
+#include <Pancake/Graphics/Vertex.hpp>
+
+#endif // GRAPHICS_HPP
---
+++ include/Pancake/Window.hpp
@@ -0,0 +1,6 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+#include <Pancake/Window/Window.hpp>
+
+#endif // WINDOW_HPP
