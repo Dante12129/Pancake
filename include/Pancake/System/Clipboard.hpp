770d234af58db75aa1bcb5e7e5805128512a388b
Dante12129 <wolfdan@cox.net>
2014-11-22 14:04:01 -0500

    Complete Clipboard Methods

--- include/Pancake/System/Clipboard.hpp
+++ include/Pancake/System/Clipboard.hpp
@@ -1,11 +1,16 @@
 #ifndef CLIPBOARD_HPP
 #define CLIPBOARD_HPP
 
+#include <string>
+
 namespace pcke
 {
     class Clipboard
     {
         public:
+            static bool hasText();
+            static std::string getText();
+            static void setText(const std::string& text);
     };
 }
 
--- source/System/Clipboard.cpp
+++ source/System/Clipboard.cpp
@@ -1,5 +1,37 @@
 #include "include\Pancake\System\Clipboard.hpp"
 
+#include <iostream>
+
+#include <SDL2/SDL_clipboard.h>
+#include <SDL2/SDL_error.h>
+
 namespace pcke
 {
+    bool Clipboard::hasText()
+    {
+        return SDL_HasClipboardText();
+    }
+    std::string Clipboard::getText()
+    {
+        auto mem = SDL_GetClipboardText();
+        if(!mem)
+        {
+            std::cerr << "Error getting clipboard text: " << SDL_GetError() << std::endl;
+            SDL_free(mem);
+            return "";
+        }
+        else
+        {
+            std::string text(mem);
+            SDL_free(mem);
+            return text;
+        }
+    }
+    void Clipboard::setText(const std::string& text)
+    {
+        if(SDL_SetClipboardText(text.c_str()))
+        {
+            std::cerr << "Error setting clipboard text: " << SDL_GetError() << std::endl;
+        }
+    }
 }
