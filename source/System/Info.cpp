9c02dc3f60bc8b00bb946c14b9483915eedf7ef8
Dante12129 <wolfdan@cox.net>
2014-11-22 22:37:36 -0500

    Implement CPU Class

--- source/System/Info.cpp
+++ source/System/Info.cpp
@@ -1,5 +1,6 @@
 #include "include\Pancake\System\Info.hpp"
 
+#include <SDL2/SDL_cpuinfo.h>
 #include <SDL2/SDL_platform.h>
 
 namespace pcke
@@ -7,5 +8,18 @@ namespace pcke
     std::string getPlatform()
     {
         return SDL_GetPlatform();
+    }
+
+    int CPU::getCacheSize()
+    {
+        return SDL_GetCPUCacheLineSize();
+    }
+    int CPU::getThreads()
+    {
+        return SDL_GetCPUCount();
+    }
+    int CPU::getRAM()
+    {
+        return SDL_GetSystemRAM();
     }
 }
