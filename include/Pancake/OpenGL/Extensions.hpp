60d0c45e940cd7eeea1bcbb19fb7f00b985b3042
Dante12129 <wolfdan@cox.net>
2014-12-12 00:16:52 -0500

    Create Extensions Tester Prototypes
    These are all the extensions currently used or planned to be used in the
    near future.

--- include/Pancake/OpenGL/Extensions.hpp
+++ include/Pancake/OpenGL/Extensions.hpp
@@ -5,6 +5,25 @@ namespace pcke
 {
     namespace ext
     {
+        //Shader extensions
+        bool programBinary();
+        bool programInterfaceQuery();
+        bool separateShaderObjects();
+        bool glsl420Pack();
+        bool shaderSubroutine();
+        bool computeShader();
+
+        //Texture extensions
+        bool textureCompressionS3TC();
+        bool textureStorage();
+        bool clearTexture();
+
+        //Ungrouped extensions
+        bool directStateAccess();
+        bool clearBufferObject();
+        bool viewportArray();
+        bool drawIndirect();
+
     }
 }
 
