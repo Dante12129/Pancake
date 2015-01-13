52df1fd9504be6a450e07d00ba9fa29fdf3a5fe0
Dante12129 <wolfdan@cox.net>
2015-01-12 20:13:23 -0500

    Implement Library Basics
    Loading hasn't been implemented as it will take a bit more work to
    handle all platforms.

--- include/Pancake/System/Library.hpp
+++ include/Pancake/System/Library.hpp
@@ -1,10 +1,19 @@
 #ifndef LIBRARY_HPP
 #define LIBRARY_HPP
 
+#include <string>
+
 namespace pcke
 {
     class Library
     {
+        public:
+            ~Library();
+
+            void* getSymbol(const std::string& name);
+
+        private:
+            void* lib = nullptr;
     };
 }
 
--- source/System/Library.cpp
+++ source/System/Library.cpp
@@ -1,5 +1,27 @@
 #include "include/Pancake/System/Library.hpp"
 
+#include <iostream>
+
+#include <SDL2/SDL_loadso.h>
+
 namespace pcke
 {
+    Library::~Library()
+    {
+        if(lib)
+        {
+            SDL_UnloadObject(lib);
+        }
+    }
+
+    void* Library::getSymbol(const std::string& name)
+    {
+        auto func = SDL_LoadFunction(lib, name.c_str());
+        if(!func)
+        {
+            std::cerr << "Error getting a function from library: " << SDL_GetError() << std::endl;
+            return nullptr;
+        }
+        return func;
+    }
 }
